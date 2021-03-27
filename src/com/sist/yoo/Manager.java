18cbfd3c50f8aaf86a758d230caa822736ebd49d
rachel5004 <j26597719@naver.com>
2021-03-27 17:31:09 +0900

    Idus:: (update) add columns

--- src/com/sist/yoo/Manager.java
+++ src/com/sist/yoo/Manager.java
@@ -12,8 +12,6 @@ public class Manager {
 //			System.out.println(doc);
 			
 			Elements id = doc.select("li.ui_grid__item button.ui_card__overlay");
-			Elements loc = doc.select("li.ui_grid__item div.ui_card--white span.ui_card__overlay--label");
-			Elements image = doc.select("li.ui_grid__item div.ui_card--white div.ui_card__imgcover a.ui_card__img");
 			
 			Elements link = doc.select("picture.img img");
 			for(int i=0;i<id.size();i++) {
@@ -24,10 +22,21 @@ public class Manager {
 				
 				Elements title = target.select("div.sticky_aside_product h2.sticky_aside__produc-title");
 				Elements artist = target.select("div.sticky_aside_product span.artist_card__label");
-
+				Elements image = target.select("ul.img-view li.ui-slide");
+				Elements price = target.select("strong.sold-price");
+				Elements detail = target.select("div.sticky_aside_product span.artist_card__label");
+				Elements category = target.select("a.txt-strong");
+				
+				
+				String imgstr = image.get(0).attr("style");
+				
 				for(int j=0;j<title.size();j++) {
 					System.out.println("product: "+title.get(j).text());
 					System.out.println("artist: "+artist.get(j).text());
+					System.out.println("image: "+imgstr.substring(imgstr.indexOf("(")+1,imgstr.indexOf(")")));
+//					System.out.println("price: "+price.get(j).text());
+					System.out.println(price.size());
+					System.out.println("category: "+category.get(j).text());
 				}
 				
 				k++;
