5ff711f08d9596230a41e28ec4f7db547201e5c4
JunHo <u_innovation@naver.com>
2021-03-29 21:19:18 +0900

    Idus online

--- src/com/sist/jun/Manager.java
+++ src/com/sist/jun/Manager.java
@@ -7,56 +7,63 @@ import org.jsoup.select.Elements;
 public class Manager {
 	public void productData() {
 		try {
-			int k=1;
-			int page = 8;
-			// 아이디어스 온라인 클래스
-			Document doc = Jsoup.connect("https://www.idus.com/oc/class/"+page).get();
-			
-			// System.out.println(doc);
-			
-			
-			for(int i=0;i<1;i++) {
-				System.out.println("number: "+k);
-				
-				// 아이디어스 온라인 클래스 상세페이지
-				Elements artist = doc.select("span.artist_card__label");
-				Elements subject = doc.select("small.sticky_aside__small");
-				Elements title = doc.select("h2.sticky_aside__title");
-				Elements image = doc.select("img.article_block__img");
-				Elements context1 = doc.select("h3.article_block__title");
-				Elements context2 = doc.select("p.article_block__para");
-				String imgstr = image.get(i).attr("src");
-				
-//				String pricestr = String.valueOf(price.get(0));
-//				String dataprice = "data-sold-price";
-//				int idx = pricestr.indexOf(dataprice)+17;
-//				String pslide = pricestr.substring(idx,idx+10);
-//				String pslide2 = pslide.substring(0,pslide.lastIndexOf("\""));
-				
-				for(int j=0;j<artist.size();j++) {
-					try {
-						System.out.println("artist: "+artist.get(j).text());
-						System.out.println("subject:"+subject.get(j).text());
-						System.out.println("product: "+title.get(j).text());
-						System.out.println("context1: "+context1.get(j).text());
-						System.out.println("context2: "+context2.get(j).text());
-						System.out.println("image: "+imgstr);
+			int k = 1;
+			for (int p = 8; p <= 207; p++) {
+				try {
+					// 아이디어스 온라인 클래스
+					Document doc = Jsoup.connect("https://www.idus.com/oc/class/" + p).get();
+
+					// System.out.println(doc);
+
+					
+					System.out.println("number: " + k);
+
+					// 아이디어스 온라인 클래스 상세페이지
+					Elements artist = doc.select("span.artist_card__label");
+					Elements subject = doc.select("small.sticky_aside__small");
+					Elements title = doc.select("h2.sticky_aside__title");
+					Elements image = doc.select("img.article_block__img");
+					Elements context1 = doc.select("h3.article_block__title");
+					Elements context2 = doc.select("p.article_block__para");
+
+					for (int j = 0; j < artist.size(); j++) {
+						try {
+							System.out.println("artist: " + artist.get(j).text());
+							System.out.println("subject:" + subject.get(j).text());
+							System.out.println("product: " + title.get(j).text());
+							System.out.println("context1: " + context1.get(j).text());
 						
-						
-						//						System.out.println("price: "+ pslide2 + "원");
-//						System.out.println("category: "+category.get(j).text());
-					}catch(Exception e) {
-						System.out.println("null");
+						} catch (Exception e) {
+							System.out.println("null");
+						}
 					}
+					String contents = "";
+					String img="";
+							
+					for (int j = 0; j < context2.size(); j++) {
+						img += (image.get(j).attr("src")+"^");
+						contents+= (context2.get(j).text()+"^");
+					}
+					System.out.println(img);
+					System.out.println(contents);
+					
+					k++;
+					
+					
+				}catch(Exception ex) {
+//					ex.printStackTrace();
+					System.out.println("나와랏");
 				}
 				
-				k++;
-				page++;
 			}
-		}catch (Exception e) {
-			e.printStackTrace();
+			
+		} catch (Exception e) {
+//			e.printStackTrace();
+			System.out.println("no page");
 		}
+		
 	}
+
 	public static void main(String[] args) {
 		Manager fm = new Manager();
 		fm.productData();
