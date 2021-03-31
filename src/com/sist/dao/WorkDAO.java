add83269e5d06a3ef2bb7a0c4e9c9fbee71f9a48
HP <HP@DESKTOP-PMO008M>
2021-03-31 22:59:28 +0900

    차나리

--- src/com/sist/dao/WorkDAO.java
+++ src/com/sist/dao/WorkDAO.java
@@ -1,5 +1,6 @@
 package com.sist.dao;
 
 public class WorkDAO {
+	
 
 }
--- src/com/sist/vo/WorkVO.java
+++ src/com/sist/vo/WorkVO.java
@@ -1,5 +1,15 @@
 package com.sist.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WorkVO {
+	private int no;
+	private String poster;
+	private String artist;
+	private String title;
+	
 
 }
