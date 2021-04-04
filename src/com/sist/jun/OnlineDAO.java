961ebcae7adc3c1e5767ba6eb70fa5bf38e335ea
rachel5004 <j26597719@naver.com>
2021-04-05 02:50:35 +0900

    .

--- src/com/sist/jun/OnlineDAO.java
+++ src/com/sist/jun/OnlineDAO.java
@@ -8,30 +8,34 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 import com.sist.dao.CartDAO;
+import com.sist.jdbc.DAOManager;
 import com.sist.jun.OnlineVO;
-
+import com.sist.vo.WorkVO;
 
 public class OnlineDAO {
+	private DAOManager dm = new DAOManager();
 	private Connection conn;
-	   private PreparedStatement ps;
-	   private static OnlineDAO dao;
-	   // 미리 만들어진 객체주소를 얻어온다 (Connection)
-	   public void getConnection() {
-		   try {
-			   Context init=new InitialContext();
-			   Context c=(Context)init.lookup("java://comp/env");
-			   DataSource ds=(DataSource)c.lookup("jdbc/oracle");
-			   conn=ds.getConnection();
-		   }catch(Exception ex){
-
-		   }
-	   }
-	   public void disConnection() {
-		   try {
-			   if(ps!=null) ps.close();
-			   if(conn!=null) conn.close();
-		   }catch(Exception ex) {}
-	   }
+	private PreparedStatement ps;
+	public void onlineDataInsert(OnlineVO vo) {
+		 try {
+			 conn=dm.getConnection();
+			 String sql = "INSERT INTO testclass(c_no, c_title, c_content, c_poster, c_artist, c_price,c_onoff) VALUES "
+				+ "((SELECT NVL(MAX(c_no)+1,1) FROM testclass),?,?,?,?,?,0)";
+				ps=conn.prepareStatement(sql);
+				ps.setString(1, vo.getCtitle());
+				ps.setString(2, vo.getCcontent());
+				ps.setString(3, vo.getCposter());
+				ps.setString(4, vo.getCartist());
+				ps.setString(5, vo.getCprice());
+				
+				ps.executeUpdate();
+		 }catch(Exception ex) {
+			 ex.printStackTrace();
+		 } finally {
+			 dm.disConnection(conn, ps);
+		 }
+	 
+	}
 	/*
 	 * C_NO        NOT NULL NUMBER        
 		C_TITLE              VARCHAR2(300) 
@@ -45,27 +49,27 @@ public class OnlineDAO {
 		C_CATEGORY           VARCHAR2(50)  
 	 * 
 	 */
-	public void onlineDataInsert(OnlineVO vo) {
-		try {
-			getConnection();
-			String sql = "INSERT INTO testclass(c_no, c_title, c_content, c_poster, c_artist, c_price,c_onoff) VALUES "
-					+ "((SELECT NVL(MAX(c_no)+1,1) FROM testclass),?,?,?,?,?,0)";
-			   ps=conn.prepareStatement(sql);
-			   ps.setString(1, vo.getCtitle());
-			   ps.setString(2, vo.getCcontent());
-			   ps.setString(3, vo.getCposter());
-			   ps.setString(4, vo.getCartist());
-			   ps.setString(5, vo.getCprice());
-			   
-			   ps.executeUpdate();
-			   
-		}catch(Exception ex) {
-			ex.printStackTrace();
-		}finally {
-			disConnection();
-		}
-		
-		
-	}
-	
+//	public void onlineDataInsert(OnlineVO vo) {
+//		try {
+//			getConnection();
+//			String sql = "INSERT INTO testclass(c_no, c_title, c_content, c_poster, c_artist, c_price,c_onoff) VALUES "
+//					+ "((SELECT NVL(MAX(c_no)+1,1) FROM testclass),?,?,?,?,?,0)";
+//			   ps=conn.prepareStatement(sql);
+//			   ps.setString(1, vo.getCtitle());
+//			   ps.setString(2, vo.getCcontent());
+//			   ps.setString(3, vo.getCposter());
+//			   ps.setString(4, vo.getCartist());
+//			   ps.setString(5, vo.getCprice());
+//			   
+//			   ps.executeUpdate();
+//			   
+//		}catch(Exception ex) {
+//			ex.printStackTrace();
+//		}finally {
+//			disConnection();
+//		}
+//		
+//		
+//	}
+//	
 }
