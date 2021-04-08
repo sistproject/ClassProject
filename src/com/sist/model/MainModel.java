b4a2e9804eb69fc137f24ce9c5d09ed92b53e80f
gitshinyohan <dygkstls@gmail.com>
2021-04-08 12:51:03 +0900

    메인모델

--- src/com/sist/model/MainModel.java
+++ src/com/sist/model/MainModel.java
@@ -1,15 +1,23 @@
 package com.sist.model;
 
+import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
+import com.sist.dao.ClassDAO;
 @Controller
 public class MainModel {
 	@RequestMapping("main/main.do")
 	public String login(HttpServletRequest request,HttpServletResponse response) {
 		String name = "";
+		
+		ClassDAO dao = ClassDAO.newInstance();
+		List<String> catList = dao.getCategory();
+		int catCount = dao.getCategoryCount();
+		request.setAttribute("catList", catList);
+		request.setAttribute("catCount", catCount);
 		request.setAttribute("name", name);
 		request.setAttribute("main_jsp", "../main/home.jsp");
 		request.setAttribute("menu", "work");
@@ -19,6 +27,12 @@ public class MainModel {
 	@RequestMapping("main/cmain.do")
 	public String classPage(HttpServletRequest request,HttpServletResponse response) {
 		String name = "";
+		
+		ClassDAO dao = ClassDAO.newInstance();
+		List<String> catList = dao.getCategory();
+		int catCount = dao.getCategoryCount();
+		request.setAttribute("catList", catList);
+		request.setAttribute("catCount", catCount);
 		request.setAttribute("name", name);
 		request.setAttribute("main_jsp", "../main/home.jsp");
 		request.setAttribute("menu", "class");
