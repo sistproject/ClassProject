c6e36f179d11df4921242d45ec5430f4cb7a94ff
JunHo <u_innovation@naver.com>
2021-04-08 14:43:13 +0900

    workModel오류 수정

--- src/com/sist/model/WorkModel.java
+++ src/com/sist/model/WorkModel.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.connector.Request;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
