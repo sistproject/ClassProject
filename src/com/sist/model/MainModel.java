64bcf4fc2eacf179c18250a49686d9b9c756e3a0
JunHo <u_innovation@naver.com>
2021-04-08 15:19:58 +0900

    ?

--- src/com/sist/model/MainModel.java
+++ src/com/sist/model/MainModel.java
@@ -1,12 +1,18 @@
 package com.sist.model;
 
 import java.util.*;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
 import com.sist.dao.ClassDAO;
+import com.sist.dao.WorkDAO;
+import com.sist.vo.WorkDetailVO;
+import com.sist.vo.WorkVO;
 @Controller
 public class MainModel {
 	@RequestMapping("main/main.do")
@@ -38,6 +44,48 @@ public class MainModel {
 		request.setAttribute("menu", "class");
 		return "../main/main.jsp";
 	}
+	@RequestMapping("main/main.do")
+	public String main_home(HttpServletRequest request,HttpServletResponse response) {
+		
+        // 쿠키
+		List<WorkDetailVO> fList=new ArrayList<WorkDetailVO>();
+		System.out.println("=================rr=========");
+		WorkDAO dao=new WorkDAO();
+		Cookie[] cookies=request.getCookies();			
+		  if(cookies != null)
+		  {
+			  for(int i=cookies.length-1;i>=0;i--)
+			  {
+				  System.out.println("쿠키 포문");
+				  if(cookies[i].getName().startsWith("m"))
+				  { 	
+					  System.out.println("쿠키 조건문");
+					  cookies[i].setPath("/");
+					  System.out.println(cookies[i].getName()); // key
+					  String w_no=cookies[i].getValue(); // value
+					  System.out.println(cookies[i].getValue());
+					  WorkDetailVO vo=dao.WorkCookiePrintData(Integer.parseInt(w_no));
+					  System.out.println(vo.getW_no());
+					  fList.add(vo);
+					  
+	
+				  }
+			  }
+		  }
+		request.setAttribute("fList", fList);
+		System.out.println("============tt=====rr=========");
+		//String w_no=request.getParameter("w_no");
+		List<WorkVO> wList=dao.WorkMainData();
+		request.setAttribute("wList", wList);
+		//request.setAttribute("menu", "work");
+		request.setAttribute("main_jsp", "../main/home.jsp");
+
+		return "../main/main.jsp";
+
+	}
+
+
+
 
 
 }
